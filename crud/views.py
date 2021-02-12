a016bfe3a34efc2c2f733a32c7842bb276995a84
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-02-12 18:00:09 +0100

    first commit
    crud creation

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse as http
 
 # Create your views here.
+
+def home(request):
+    return http('<h1>At the beguining all was darkness!</h1>')
+
--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Views import views
+
+from crud import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
 ]
